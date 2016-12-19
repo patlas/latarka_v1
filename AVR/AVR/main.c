7a5d898a538916699cc46dbb607713cb02156706
Patryk Łaś <patryk.las@comarch.com>
2016-12-19 13:37:23 +0100

    Some changes from drive - to be tested. Safety commit

--- AVR/AVR/main.c
+++ AVR/AVR/main.c
@@ -30,49 +30,50 @@ volatile uint8_t adc_channel; //??
 typedef struct adc_t {
 	uint8_t channel;
 	uint8_t *OCR;
+	uint8_t pwm_val;
 }adc_t;
 
 volatile adc_t adc[2];
 
 
 volatile uint8_t index = 0;
-volatile uint8_t m_index = 0;
-volatile uint16_t adc_val = 0;
-volatile uint8_t pwm_val = PWM_MAX/2;
+volatile uint8_t m_index[2] = {0};
+volatile uint16_t adc_val[2] = {0};
+//volatile uint8_t pwm_val = PWM_MAX/2;
 
 ISR(ADC_vect)
 {
 	//uint16_t adc_val = ADCL;
 	//adc_val |= ((ADCH<<8) & 0xF00);
-	adc_val += ADC;
-	m_index++;
-	adc_val /= 2;//m_index; 
+	adc_val[index] += ADC;
+	m_index[index]++;
+	adc_val[index] /= 2;//m_index; 
 
-	if(m_index >= 10)
+	if(m_index[index] >= 10)
 	{
-		m_index = 0; /// odwrotna logika!!!!
-		if(adc_val < ADC_COMP_VAL)
+		m_index[index] = 0; /// odwrotna logika!!!!
+		if(adc_val[index] < ADC_COMP_VAL)
 		{
-			pwm_val -= STEP;
-			if(pwm_val < STEP)
-				pwm_val = STEP;
-			*(adc[index].OCR) = pwm_val;
+			adc[index].pwm_val -= STEP;
+			if(adc[index].pwm_val < STEP)
+				adc[index].pwm_val = STEP;
+			*(adc[index].OCR) = adc[index].pwm_val;
 			//PORTB |= 1<<3;
 		}
-		else if(adc_val > ADC_COMP_VAL)
+		else if(adc_val[index] > ADC_COMP_VAL)
 		{
-			pwm_val += STEP;
-			if(pwm_val > PWM_MAX)
-				pwm_val = PWM_MAX;
-			*(adc[index].OCR) = pwm_val;
+			adc[index].pwm_val += STEP;
+			if(adc[index].pwm_val > PWM_MAX)
+				adc[index].pwm_val = PWM_MAX;
+			*(adc[index].OCR) = adc[index].pwm_val;
 			//PORTB &= ~(1<<3);
 		}
 		//adc_val = 0;
 	}
 	//if(++index > 1)
 	//	index = 0;
-
-	//adc_set_channel(adc[index].channel); // wait to correct channel change -> no free running mode -> manual trigger in interrupt!!!
+	index ^= 0x01;
+	adc_set_channel(adc[index].channel); // wait to correct channel change -> no free running mode -> manual trigger in interrupt!!!
 	adc_start();
 }
 
@@ -82,14 +83,16 @@ int main(void)
 
 	adc[0].channel = 1;
 	adc[0].OCR = &OCR1A;
+	adc[0].pwm_val = 0;
 	adc[1].channel = 3;
 	adc[1].OCR = &OCR1B;
+	adc[1].pwm_val = 0;
 
 	adc_channel = 0;
 
-	DDRB |=  1<<1 | 1<<0 | 1<<3;
-	DDRB &= ~(1<<2);
-	PORTB &= ~(1<<3);
+	DDRB |=  1<<1 | 1<<0 /*| 1<<3*/ | 1<<4;
+	DDRB &= ~(1<<2 | 1<<3);
+	//PORTB &= ~(1<<3);
 	_delay_ms(500);
 
 	pwm_init();
@@ -98,12 +101,12 @@ int main(void)
 	
 
 	PORTB &= ~(1<<0);
-	PORTB |= (1<<3);
+	//PORTB |= (1<<3);
 
 
 	//OCR1A = 110;
 	uint16_t x = 0;//ADC;
-	adc_set_channel(1);
+	adc_set_channel(adc[index].channel);
 	adc_start();
 	sei();
 
